454ba20a6e783ef7c045294c2f682766ebbd3579
ludogwada <83616314+ludogwada@users.noreply.github.com>
2022-10-26 08:17:10 -0400

    feat pages : add hebergement

--- src/pages/Hebergement.js
+++ src/pages/Hebergement.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import Collapse from '../components/collapse/Collapse';
+import Slider from '../components/slider/slider';
+import DescriptionBungalows from '../data/description-bungalow.json';
+import Header from '../layout/Header';
 
 const Hebergement = () => {
-	return <div>Hebergement</div>;
+	const { hebergementId } = useParams();
+	const [hebergementData] = useState(DescriptionBungalows);
+
+	let hebergement = hebergementData.find(
+		(hebergement) => hebergement.id === hebergementId
+	);
+	if (!hebergement) {
+		return <Navigate to='*' />;
+	}
+
+	return (
+		<main>
+			<Header />
+			<article className='hebergement'>
+				<h1>{hebergement.title}</h1>
+				<section className='hebergement__description'>
+					<Slider pictures={hebergement.pictures} title={hebergement.title} />
+					<h2 className='hebergement__description__text'>
+						{hebergement.description}
+					</h2>
+				</section>
+				<section className='hebergement__equipement'>
+					<Collapse
+						title='Equipement'
+						description={hebergement.equipments.map((equipment, index) => (
+							<li key={index}>{equipment}</li>
+						))}
+					/>
+				</section>
+			</article>
+		</main>
+	);
 };
 
 export default Hebergement;
